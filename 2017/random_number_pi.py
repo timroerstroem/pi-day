629ff7dc4895e51449475188f0045bdbd0fa227e
timroerstroem <timroerstroem@gmail.com>
2017-03-14 12:34:22 +0100

    Code optimisation.

--- 2017/random_number_pi.py
+++ 2017/random_number_pi.py
@@ -5,29 +5,22 @@ https://www.youtube.com/watch?v=RZBhSi_PwHU
 
 '''
 import numpy
-import math
 
 
 def gcd(a, b):
     if a == b:
         return a
     elif a < b:
-        guess = a
-        largest = b
+        lo = a
+        hi = b
     else:
-        guess = b
-        largest = a
-
+        lo = b
+        hi = a
     while True:
-        if largest - guess == guess:
-            return guess
-        elif largest - guess > guess:
-            largest = largest - guess
-            continue
+        if hi % lo == 0:
+            return lo
         else:
-            temp = largest - guess
-            largest = guess
-            guess = temp
+            hi, lo = lo, hi % lo
             continue
 
 
@@ -36,16 +29,14 @@ Convert input to float and then to int. It's ugly, but it allows the user to
 input numbers in scientific notation.
 '''
 limit = numpy.int64(float(input("Highest integer to use: ")))
-num = numpy.int64(float(input("Number of integers to use: ")))
+num = numpy.int64(float(input("Number of integer pairs to use: ")))
 numar = numpy.random.randint(1, high=limit, size=(2, num), dtype='int64')
 coprime = []
 
 for i in range(len(numar[0])):
     if gcd(numar[0][i], numar[1][i]) == 1:
         coprime.append(1)
-    else:
-        coprime.append(0)
 
-prob = sum(coprime)/len(coprime)
+prob = len(coprime)/num
 
-print("Pi is approximately " + repr(math.sqrt(6/prob)))
+print("Pi is approximately " + repr(numpy.sqrt(6/prob)))
